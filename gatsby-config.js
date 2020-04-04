a827fd69bdd649feeeb41d201e28ff25b6527373
sangeet <sangeet.khatri@protonmail.com>
2020-04-04 21:32:46 +0530

    Added favicon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,13 +50,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ilaana-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fbb6ce`,
+        theme_color: `#fbb6ce`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
